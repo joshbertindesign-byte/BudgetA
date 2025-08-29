9f1e16fef833071ef171be7c4a733e04f82227db
joshbertindesign-byte <joshbertindesign@gmail.com>
2025-08-29 13:45:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1034,14 +1034,3 @@ export default function App() {
         </div>
     );
 }
-
-
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
